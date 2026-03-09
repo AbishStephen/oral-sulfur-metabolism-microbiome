348980c15ecfeded01edf0ab9504db8254bdbcdd
AbishStephen <104612385+AbishStephen@users.noreply.github.com>
2026-03-09 09:35:33 +0000

    Create 01_load_data.R

---
+++ scripts/01_load_data.R
@@ -0,0 +1,7 @@
+library(tidyverse)
+
+clinical_data <- read_csv("data/clinical_data.csv")
+gcf_cytokines <- read_csv("data/gcf_cytokines.csv")
+microbial_data <- read_csv("data/microbial_data.csv")
+sample_info <- read_csv("data/sampleinfo.csv")
+qpcr <- read_csv("data/qpcr_data.csv")
